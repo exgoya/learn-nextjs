9ff22ff12654fa227bd115e55c5d4635ab12eb86
exgoya <bam125@naver.com>
2024-06-10 11:28:48 +0900

    add work

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -10,9 +10,12 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   const resp = await fetch(process.env.NEXT_PUBLIC_API_URL+'topics', {cache: 'no-cache'});
   const topics = await resp.json();
+  const resp1 = await fetch(process.env.NEXT_PUBLIC_BACKEND_API_URL+"api");
+  const data1 = await resp1.json();
   return (
     <html>
       <body>
+        <h1>{data1.message}</h1>
         <h1><Link href="/">WEB</Link></h1>
         <ol>
           {topics.map((topic)=>{
